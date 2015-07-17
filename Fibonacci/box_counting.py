53db46887d0bb68206c6cb4b2f68a9be6193f46b
Nicolas Macé <nicolas91mace@gmail.com>
2015-07-17 18:16:07 +0200

    corrected notebook, edited box_counting for comparison btw co and normal numerotation box counting

--- Fibonacci/box_counting.py
+++ Fibonacci/box_counting.py
@@ -115,8 +115,10 @@ om = 2./(1.+math.sqrt(5))
 
 # jump amplitudes
 def jump(i, tw, ts):
-    fibi = int(om*(i+1)) - int(om*i) # an integer taking values 0 or 1 occordingly to Fibonacci sequence
-    if(fibi < 1):return ts
+    p = fib(n-2)    
+    q = fib(n-1)
+    L = fib(n)    
+    if(i*q % L < p):return ts
     else:return tw
 
 # build the hamiltonian (free boundary conditions)
@@ -124,7 +126,7 @@ def h(n, rho):
     L = fib(n)
     h = np.zeros((L,L))
     for i in range(L-1): 
-        h[i,i+1] = jump(i+1, rho, 1.)
+        h[i,i+1] = jump(i, rho, 1.)
         h[i+1,i] = h[i,i+1]
     return h
     
@@ -133,32 +135,57 @@ def hp(n, rho):
     L = fib(n)    
     hp = np.zeros((L,L))
     for i in range(L-1): 
-        hp[i,i+1] = jump(i+1, rho, 1.)
+        hp[i,i+1] = jump(i, rho, 1.)
         hp[i+1,i] = hp[i,i+1]
-        hp[0,L-1] = jump(L, rho, 1.)
+        hp[0,L-1] = jump(L-1, rho, 1.)
         hp[L-1,0] = hp[0,L-1]
     return hp
+    
+""" Pass to conumbering """
+
+def conum(vec, n):
+    q = fib(n-1)
+    L = fib(n)
+    # compute the permutation matrix associated to approximant n
+    p = np.fromfunction(lambda i,j: i == q*j % L, (L, L))
+    # permute elements in vec
+    perm_vec = np.dot(p,np.dot(vec,np.transpose(p)))
+    return perm_vec
 
 # ## Local fractal dimensions of the wavefunctions
 
 """ compute the eigenvalues """
 
 # diagonalize
-rho = 0.1
-n = 19
+rho = .1
+n = 15
 val, vec = linalg.eigh(hp(n, rho))
 
-""" Averaged fractal dimensions of the wavefunctions """
-q = -10.
+# in conumbering!
+covec = conum(vec,n)
 
+""" Averaged fractal dimensions of the wavefunctions """
+q = 2.
+
+# position set
+s = np.arange(0,fib(n),1.)
+
+# wavefunction index 
+a = 200
+
+# weights are presence probabilities
+w = abs(vec[:,a])**2
+cow = abs(covec[:,a])**2
     
 # smallest box size (in the form smallest_espilon = 10**(-t))
 t = 3.5
 # for some reason, there are problems with 3, so we exclude it from the list of primes
 primes = [2,5,6,7,10]
 epsilonRange = sorted([1/n**delta for n in primes for delta in range(1,int(t*math.log(10.)/math.log(n))+1)])
+#epsilonRange = 2**np.arange(-21.,0.,1.)
 
-qwList = [av_qWeight(vec, epsilon, q) for epsilon in epsilonRange]
+qwList = [qWeight(s, w, epsilon, q) for epsilon in epsilonRange]
+coqwList = [qWeight(s, cow, epsilon, q) for epsilon in epsilonRange]
 
 """ data plot and linear regression """
 
@@ -168,7 +195,7 @@ def loglogplot(min, max):
     epsilonRange2 = epsilonRange[min:max]
     qwList2 = qwList[min:max]
 
-    plt.title('The q-weigth for the wavfunctions for q = ' + str(q))
+    plt.title('The q-weigth for the wavefunction '+str(a)+' for q = ' + str(q))
     plt.xlabel('log epsilon')
     plt.ylabel('log chi')
     plt.loglog(epsilonRange, qwList,'-,r',markersize=3., linewidth=2.)
@@ -181,41 +208,48 @@ def loglogplot(min, max):
 
     slope, intercept, r_value, p_value, std_err = stats.linregress(logEps,logQW)
     print(slope, r_value)
-    
-""" iterate over q """
 
-## smallest box size (in the form smallest_espilon = 10**(-t))
-##t = 2.5
-#t = 3.5
-#primes = [2,5,7,10]
-#epsilonRange = sorted([1/n**delta for n in primes for delta in range(1,int(t*math.log(10.)/math.log(n))+1)])
-##epsilonRange = epsilonRange[3:-1]
-#epsilonRange = epsilonRange[8:-1]
-#
-## range of values of q
-#qRange = np.arange(-10.,10.,0.1)
-#    
-## compute tau
-#def tau(q):
-#    qwList = [av_qWeight(vec, epsilon, q) for epsilon in epsilonRange]
-#    
-#    # regression
-#    logQW = [math.log(qw) for qw in qwList]
-#    logEps = [math.log(eps) for eps in epsilonRange]
-#
-#    tau, intercept, r_value, p_value, std_err  = stats.linregress(logEps,logQW)
-#    return tau
-#
-## launch kernels
-#pool = Pool(4)
-#
-## parallel mapping
-#lin_reg = list(pool.map(tau, qRange))
-#    
-##dq = [tau/(q-1) for q, tau in zip(qRange, lin_reg)]
-#
-#""" saving to a file """
-#
-#qtau = [list(it) for it in zip(qRange,lin_reg)]
-## save to a file readable by Mathematica using Import and by Python using np.loadtxt
-#np.savetxt('data/tauqpsi_python_rho_'+str(rho)+'_n_'+str(n)+'.dat',qtau)
+def cologlogplot(min, max):
+    
+    # plot
+    epsilonRange2 = epsilonRange[min:max]
+    qwList2 = coqwList[min:max]
+
+    plt.title('The q-weigth for the wavefunction '+str(a)+' for q = ' + str(q))
+    plt.xlabel('log epsilon')
+    plt.ylabel('log chi')
+    plt.loglog(epsilonRange, coqwList,'-,r',markersize=3., linewidth=2.)
+    plt.loglog(epsilonRange2, qwList2,'o',markersize=3., linewidth=2.)
+    plt.show()
+    
+    # regression
+    logQW = [math.log(qw) for qw in qwList2]
+    logEps = [math.log(eps) for eps in epsilonRange2]
+
+    slope, intercept, r_value, p_value, std_err = stats.linregress(logEps,logQW)
+    print(slope, r_value)
+    
+def comp(min, max):
+    
+    # plot
+    epsilonRange2 = epsilonRange[min:max]
+    coqwList2 = coqwList[min:max]
+    qwList2 = qwList[min:max]
+
+    plt.title('The q-weigth for the wavefunction '+str(a)+' for q = ' + str(q))
+    plt.xlabel('log epsilon')
+    plt.ylabel('log chi')
+    plt.loglog(epsilonRange, qwList,'-',markersize=3., linewidth=2.)
+    plt.loglog(epsilonRange, coqwList,'-',markersize=3., linewidth=2.)
+    plt.loglog(epsilonRange2, qwList2,'o',markersize=3., linewidth=2.)
+    plt.loglog(epsilonRange2, coqwList2,'o',markersize=3., linewidth=2.)
+    plt.show()
+    
+    # regression
+    logQW = [math.log(qw) for qw in qwList2]
+    cologQW = [math.log(qw) for qw in coqwList2]
+    logEps = [math.log(eps) for eps in epsilonRange2]
+
+    slope, intercept, r_value, p_value, std_err = stats.linregress(logEps,logQW)
+    coslope, intercept, r_value, p_value, std_err = stats.linregress(logEps,cologQW)    
+    print(slope, coslope)
